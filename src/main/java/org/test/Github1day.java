dc816951751cfe2115d9a3361310c3d294fc6f17
Dilfer123 <dilfernisha@gmail.com>
2022-12-21 13:41:27 +0530

    jaskxga

--- src/main/java/org/test/Github1day.java
+++ src/main/java/org/test/Github1day.java
@@ -4,16 +4,6 @@ public class Github1day {
 	
 	private void name() {
 		System.out.println("Mohan");
-		System.out.println("hi");
-		System.out.println("ok");
-		System.out.println("ok");
-		System.out.println("ok");
-		System.out.println("ok");
-		System.out.println("ok");
-		System.out.println("ok");
-		System.out.println("ok");
-		System.out.println("ok");
-		System.out.println("ok");
 		System.out.println("ok");
 }
 }
